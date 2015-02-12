ee78dd06b75790d980b4abfbe34fb5f7439412a2
Yilun Liu <yil171@ucsd.edu>
2015-02-12 00:38:07 -0800

    add send message functionalities

--- client/chats/chatPage.js
+++ client/chats/chatPage.js
@@ -6,7 +6,39 @@ Template.messageItem.helpers({
 
 Template.chatPage.helpers({
 	messages: function(){
-		console.log('find');
 		return Messages.find();
 	}
 });
+
+Template.chatPage.rendered = function () {
+	$("#message-field").keypress(function(e){
+		if (e.keyCode == '13'){
+			e.preventDefault();
+			sendMessage();
+		}
+	});
+};
+
+Template.chatPage.events({
+	'click #send-message-btn': function (e) {
+		e.preventDefault();
+		sendMessage();
+	}	
+});
+
+var sendMessage = function(){
+	var content = $('#message-field').val();
+	if (content){
+		var message = {
+			content: content,
+			timeCreated: new Date(),
+			author: Meteor.user().username
+		};
+		Messages.insert(message);
+		$('#message-field').val('');
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+	else{
+		return;
+	}
+}
