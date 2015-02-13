3745fd21058202e2e5b3995fe5cf77f31bc47d78
Yilun Liu <yil171@ucsd.edu>
2015-02-13 00:02:53 -0800

    Reivew Error Check

--- client/reviews/reviewPage.js
+++ client/reviews/reviewPage.js
@@ -3,7 +3,26 @@ Template.reviewPage.helpers({
 		var sentencesArray = [];
 		for (var key in this.sentences ){
 			var sentence = {
+				key : key,
+				content: this.sentences[key]
 			}
+			sentencesArray.push(sentence);
 		}
+		return sentencesArray;
+	},
+	getTags: function(){
+		var tagsArray = [];
+		for (var i in this.tags){
+			tagsArray.push(this.tags[i]);
+		}
+		return tagsArray.join(' ');
+	}
+});
+
+Template.reviewPage.events({
+	'click button': function (e, template) {
+		e.preventDefault();
+
+		Chats.findOne({$or: [{userA: this.authorId}, {userB: this.authorId}]});
 	}
 });
