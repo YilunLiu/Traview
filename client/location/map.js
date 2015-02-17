3875ab0460043c4700177d8f6077fd7f09c4b294
Yilun Liu <yil171@ucsd.edu>
2015-02-16 17:51:35 -0800

    persitent add accross web tabs

--- client/location/map.js
+++ client/location/map.js
@@ -9,12 +9,32 @@ Template.map.rendered = function () {
         function(){
             var mapOptions = {
                 zoom: 13,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                draggable: false,
+                zoomControl: false,
+                scrollwheel: false,
+                disableDoubleClickZoom: false,
+                mapTypeControl: false,
+                streetViewControl:false,
+                scaleControl: false
+
             };
             map = new google.maps.Map(document.getElementById("googleMap"), mapOptions); 
             var center = new google.maps.LatLng( 32.8801705, -117.232095 );
             map.setCenter(center);
-            marker = new google.maps.Marker({map: map, position: center})
+
+            var markerInfo = Session.get(locationValueKey);
+            if (markerInfo){
+                var location = new google.maps.LatLng(markerInfo.location.k,markerInfo.location.D)
+                infowindow = new google.maps.InfoWindow({
+                    content: markerInfo.name
+                })
+                marker = new google.maps.Marker({map: map, position: location});
+                google.maps.event.addListener(marker, 'click', function(){
+                    infowindow.open(map,marker);
+                });
+                map.setCenter(location);
+            }
             geocoder = new google.maps.Geocoder();
         }
     );
@@ -46,7 +66,9 @@ Template.map.events({
         geocoder.geocode( {'address': address}, function(result, status){
             if (status == google.maps.GeocoderStatus.OK){
                 map.setCenter(result[0].geometry.location);
-                marker.setMap(null);
+                if (marker){
+                    marker.setMap(null);
+                }
                 marker = new google.maps.Marker({
                     map: map,
                     position: result[0].geometry.location
